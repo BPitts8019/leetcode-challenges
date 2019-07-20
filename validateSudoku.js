2e3140ae56b6d23402ed1e923d9bd527debf23da
BPitts8019 <bradleyj.pitts@gmail.com>
2019-07-19 17:20:30 -0700

    Began creating 3x3 titles with given board

--- validateSudoku.js
+++ validateSudoku.js
@@ -1,7 +1,51 @@
 /**
+ * 
+ * @param {character[][]} board
+ * @return {character[][]}
+ */
+function get3x3Tiles (board) {
+   const NUM_ITEMS = 36;
+   let rtn3x3Tiles = [];
+   let tile_3x3 = [];
+
+
+   for (let idx = 0; idx < NUM_ITEMS; idx++) {
+      console.log(`${idx % 3}, `);
+   }
+
+   console.log(board[0]);
+   return rtn3x3Tiles;
+}
+
+/**
  * @param {character[][]} board
  * @return {boolean}
  */
-var isValidSudoku = function(board) {
-    
+var isValidSudoku = function (board) {
+   const board3x3Tiles = get3x3Tiles(board);
+   let rtnVal = false;
+   
+
+
+   return rtnVal;
 };
+
+
+
+
+
+
+let test01 = [
+   ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+   ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+   [".", "9", "8", ".", ".", ".", ".", "6", "."],
+   ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+   ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+   ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+   [".", "6", ".", ".", ".", ".", "2", "8", "."],
+   [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+   [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+];
+
+
+console.log(`Test01: ${isValidSudoku(test01)}`);
